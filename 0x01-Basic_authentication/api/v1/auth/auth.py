e8e34e332d6c2af80c9c6f259dab75e02a2f5f9c
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-22 18:26:20 +0100

    authentication for paths or excluded

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,16 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method for requiring authentication
         """
-        return False
+        if not path or not excluded_paths:
+            return True
+
+        normalized_path = path.rstrip('/')
+
+        for excluded_path in excluded_paths:
+            if normalized_path == excluded_path.rstrip('/'):
+                return False
+            else:
+                return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header method"""
