f20594c904ed19b9d96b7078c5b76434f34e32cd
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-22 18:32:15 +0100

    fixed error or returning true on only first iteration of excluded path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,8 +20,8 @@ class Auth:
         for excluded_path in excluded_paths:
             if normalized_path == excluded_path.rstrip('/'):
                 return False
-            else:
-                return True
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header method"""
