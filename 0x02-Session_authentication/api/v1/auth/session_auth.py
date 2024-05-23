a71fd48953c23d0cdc5f424226dcd217b2a43826
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-23 18:47:42 +0100

    update session authentication to get session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,25 @@
 Session Authentication Module
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     Session Authentication Class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Module create a session
+        Returns:
+            session id (str)
+        """
+        if not user_id:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
