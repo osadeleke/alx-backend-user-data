c430709d629f2ecac86dae6fa310c4e482377053
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-23 03:14:43 +0100

    remove pycodestyle hide

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,7 @@ import base64
 
 class BasicAuth(Auth):
     """class for basic authentication"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa: E501
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
         get base 64 authorization header value
         """
@@ -20,7 +20,7 @@ class BasicAuth(Auth):
         else:
             return None
 
-    def decode_base64_authorization_header(self, base_64_authorization_header: str) -> str:  # noqa E501
+    def decode_base64_authorization_header(self, base_64_authorization_header: str) -> str:
         """decode base 64 authorization header
         """
         if not base_64_authorization_header:
@@ -34,7 +34,7 @@ class BasicAuth(Auth):
         except base64.binascii.Error:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):  # noqa E501
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
         """
         get user credential from basic authorization"""
         if not decoded_base64_authorization_header:
@@ -47,7 +47,7 @@ class BasicAuth(Auth):
         details = decoded_base64_authorization_header.split(':')
         return details[0], details[1]
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):  # noqa E501
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """returns the user instance based on his/her email
         and password
         """
