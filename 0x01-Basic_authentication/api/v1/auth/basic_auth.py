6ae2deeda74df16aa53359510524945dbb5c8512
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-23 03:12:14 +0100

    update basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """basic authentication module"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,4 +18,40 @@ class BasicAuth(Auth):
         if substring in authorization_header:
             return authorization_header[6:]
         else:
+            return None
+
+    def decode_base64_authorization_header(self, base_64_authorization_header: str) -> str:  # noqa E501
+        """decode base 64 authorization header
+        """
+        if not base_64_authorization_header:
+            return None
+        if not isinstance(base_64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base_64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except base64.binascii.Error:
+            return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):  # noqa E501
+        """
+        get user credential from basic authorization"""
+        if not decoded_base64_authorization_header:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+
+        details = decoded_base64_authorization_header.split(':')
+        return details[0], details[1]
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):  # noqa E501
+        """returns the user instance based on his/her email
+        and password
+        """
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
             return None
