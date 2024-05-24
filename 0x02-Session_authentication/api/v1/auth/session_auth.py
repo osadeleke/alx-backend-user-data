7e1292ec8cf27556d1bb9b9649abaeb2026dd32b
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-24 14:41:40 +0100

    get user from session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,10 @@
 """
 Session Authentication Module
 """
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -36,3 +38,15 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None):
+        """
+        Method returns a user based on cookie
+        """
+        auth_cookie = self.session_cookie(request)
+        print(f'auth cookie is {auth_cookie}')
+        auth_user_id = self.user_id_for_session_id(auth_cookie)
+        print(f'auth user id is {auth_user_id}')
+        user = User.get(auth_user_id)
+        print(user)
+        return user
