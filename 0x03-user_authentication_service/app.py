3fff796d79fa4b2ea38bf39644f7b5d4d8665cd9
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 03:18:04 +0100

    check the email exists and value error if not

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,8 +83,11 @@ def get_reset_password_token():
     """
     email = request.form.get('email')
     if email:
-        reset_token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": email, "reset_token": reset_token})
+        try:
+            reset_token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": email, "reset_token": reset_token})
+        except ValueError:
+            abort(403)
     abort(403)
 
 
