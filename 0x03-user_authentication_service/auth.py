2dc4e62af7534348a5050a49ed8ff22253622b9c
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 02:24:21 +0100

    Add: adding a session id"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,3 +62,15 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        create a session
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return user.session_id
+        except NoResultFound:
+            return None
