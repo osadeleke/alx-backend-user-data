128412e9487e782861dd26c892dada11bc9fe7e2
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 03:45:32 +0100

    move return

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -101,9 +101,10 @@ def update_password():
     new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
-    except Exception:
+    except ValueError:
         abort(403)
+    
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
