5314820bc09bbe1e92d0fd44ded9252ae790137e
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 01:34:47 +0100

    Add: checking for existence of a user before creating

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 authentication package
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -14,3 +17,23 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {user.email} already exists")
+        except NoResultFound:
+            hsh_pass = _hash_password(password)
+            new_user = self._db.add_user(email=email, hashed_password=hsh_pass)
+            return new_user
