4bfe981da772df5bf0313027f7538d96783da7f8
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 02:12:01 +0100

    generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,7 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
-from uuid import uuid4
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -24,7 +24,7 @@ def _generate_uuid():
     """
     return a string uuid
     """
-    return str(uuid4())
+    return str(uuid.uuid4())
 
 
 class Auth:
