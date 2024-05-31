84d64c84ec34db56c5ba48662830b94e637365e5
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 01:00:53 +0100

    add: update user details

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -57,3 +57,15 @@ class DB:
         if not user:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        find and update a user
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+        self._session.commit()
