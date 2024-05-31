c3b1e2f620e90014f2610bb4a6d4fad975f72493
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 02:37:27 +0100

    Add login endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,8 @@
 """
 simple flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+from flask import make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -29,6 +30,23 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response_payload = {
+            "email": email,
+            "message": "logged in"
+        }
+        response = jsonify(response_payload)
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
