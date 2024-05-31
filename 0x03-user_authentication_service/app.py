476625cd5f13da0294defab2088c314c0b69f241
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 03:39:14 +0100

    fix: changed to cover all exception

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -102,7 +102,7 @@ def update_password():
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"})
-    except ValueError:
+    except Exception:
         abort(403)
 
 
